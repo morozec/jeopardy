5d140e2fe0f6243f575c9a4413b6bb4506f90e76
morozec <andre15@inbox.ru>
2020-02-25 22:11:24 +0400

    design fixes

--- src/components/Score.js
+++ src/components/Score.js
@@ -9,15 +9,18 @@ export default function Score(props) {
 
     const playersScores = players.map((p, i) => (
         <div key={i} className={`player-info ${p.isActive ? 'is-current' : ''}`}>
+
+            {selectedQuestionIndex !== -1 && <img src={wrongImg} alt='wrong'
+                className={`pa-button ${wrongAnswerIndexes.includes(i) ? 'disabled' : ''}`}
+                onClick={() => handlePlayerAnswer(i, false)} />}
+
             <div>{p.name}</div>
             <div className='player-score'>{p.score}</div>
 
             {selectedQuestionIndex !== -1 && <img src={correctImg} alt='correct'
                 className={`pa-button ${wrongAnswerIndexes.includes(i) ? 'disabled' : ''}`}
                 onClick={() => handlePlayerAnswer(i, true)} />}
-            {selectedQuestionIndex !== -1 && <img src={wrongImg} alt='wrong'
-                className={`pa-button ${wrongAnswerIndexes.includes(i) ? 'disabled' : ''}`}
-                onClick={() => handlePlayerAnswer(i, false)} />}
+
         </div>
     ))
 
