0d95de5b52bf4f4b2f65ed7764cb92261967a1a2
morozec <andre15@inbox.ru>
2020-05-06 15:39:19 +0400

    Updates

---
+++ precache-manifest.d27d219794e96413e1d0059364790bc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aee51c3444808c860ab450da47fe862b",
+    "url": "/Jeopardy/index.html"
+  },
+  {
+    "revision": "a9e82e2afbcab6cc0688",
+    "url": "/Jeopardy/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "47da1092bc11d7085b0e",
+    "url": "/Jeopardy/static/css/main.4ef4a5b5.chunk.css"
+  },
+  {
+    "revision": "a9e82e2afbcab6cc0688",
+    "url": "/Jeopardy/static/js/2.021ba3c8.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/Jeopardy/static/js/2.021ba3c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47da1092bc11d7085b0e",
+    "url": "/Jeopardy/static/js/main.dc7fc60d.chunk.js"
+  },
+  {
+    "revision": "4d686764a48587c49f4a",
+    "url": "/Jeopardy/static/js/runtime-main.49fe1ed4.js"
+  }
+]);
