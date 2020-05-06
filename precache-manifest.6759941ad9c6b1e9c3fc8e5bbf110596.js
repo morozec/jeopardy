585ef53f27531eb5386fb5f8b3a6992a52fe8389
morozec <andre15@inbox.ru>
2020-05-06 16:16:00 +0400

    Updates

---
+++ precache-manifest.6759941ad9c6b1e9c3fc8e5bbf110596.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18c5c45e119fbcda831583e189d4387c",
+    "url": "/jeopardy/index.html"
+  },
+  {
+    "revision": "a9e82e2afbcab6cc0688",
+    "url": "/jeopardy/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "c2340a84290bcf0c630e",
+    "url": "/jeopardy/static/css/main.4ef4a5b5.chunk.css"
+  },
+  {
+    "revision": "a9e82e2afbcab6cc0688",
+    "url": "/jeopardy/static/js/2.021ba3c8.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/jeopardy/static/js/2.021ba3c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2340a84290bcf0c630e",
+    "url": "/jeopardy/static/js/main.ab094908.chunk.js"
+  },
+  {
+    "revision": "2dd67a3c34aa670daee5",
+    "url": "/jeopardy/static/js/runtime-main.73963c4c.js"
+  }
+]);
